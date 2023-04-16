520e66c87629ecf8c7f73eac9f4b15212b574918
Nazar-Smile <nazar4ik70@gmail.com>
2023-04-17 00:05:26 +0600

    third commit

--- copy.py
+++
@@ -1,8 +0,0 @@
-import os, shutil
-
-def copy(start_dir, finish_dir):
-    """Копирование файлов"""
-    copy = shutil.copy2(start_dir, os.path.join(finish_dir))
-
-
-copy("/home/n/Рабочий стол/IT_RUN/folder_test/test.py", "/home/n/Рабочий стол/IT_RUN")
