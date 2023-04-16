feb919a3aff46dd20c6ea644854287c785bd12d7
Nazar <116882052+Nazar-Smile@users.noreply.github.com>
2023-04-17 00:20:15 +0600

    Delete move.py

--- move.py
+++
@@ -1,9 +0,0 @@
-import os, shutil
-
-
-def move(start_dir, finish_dir):
-    """Перемещение файлов"""
-    move = shutil.move(start_dir, os.path.join(finish_dir))
-
-
-move("/home/n/Рабочий стол/IT_RUN/folder_test/test.py", "/home/n/Рабочий стол/IT_RUN")
